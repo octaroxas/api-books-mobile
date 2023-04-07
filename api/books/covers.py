9fb5b72a9338d3994560d31e495a352cf7131dad
Lucas Rodrigues <lucas.darlindo@gmail.com>
2023-04-07 13:13:23 -0300

    refactor: changed from ISBN to ID on Covers API

--- api/books/covers.py
+++ api/books/covers.py
@@ -7,21 +7,21 @@ class BooksCoverAPI:
     def __init__(self) -> None:
         self.ext_url = "https://covers.openlibrary.org/b/{}/{}-{}.jpg"
         self.cover_size = "L"
-        self.meta_type = "isbn"
+        self.meta_type = "ID"
 
     def set_cover_size(self, cover_size: str = "L") -> None:
         accepted_cover_sizes = ["S", "M", "L"]
         if cover_size not in accepted_cover_sizes:
-            raise ValueError(f"The informed Book Cover Size ({cover_size}) is invalid.")
+            raise ValueError(f"The informed Book Cover Size ({cover_size.upper()}) is invalid.")
 
-        self.cover_size = cover_size
+        self.cover_size = cover_size.upper()
 
-    def set_metadata_type(self, meta_type: str = "isbn") -> None:
+    def set_metadata_type(self, meta_type: str = "id") -> None:
         accepted_meta_types = ["ISBN", "OCLC", "LCCN", "OLID", "ID"]
         if meta_type not in accepted_meta_types:
-            raise ValueError(f"The informed Book MetaID Type ({meta_type}) is invalid.")
+            raise ValueError(f"The informed Book MetaID Type ({meta_type.upper()}) is invalid.")
 
-        self.meta_type = meta_type
+        self.meta_type = meta_type.upper()
 
     def get_book_cover(self, book_id: Union[str, int]) -> Optional[bytes]:
         cover_url = self.ext_url.format(self.meta_type, book_id, self.cover_size)
