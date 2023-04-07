95f4ccece557c68e4705351b01716788df6c12ae
Lucas Rodrigues <lucas.darlindo@gmail.com>
2023-04-07 16:03:05 -0300

    feat: database connection added; query method created

--- api/gateway/database.py
+++ api/gateway/database.py
@@ -1,6 +1,22 @@
 import psycopg2
 
+from typing import List
+from psycopg2.extensions import cursor
+
 
 class DatabaseAPI:
     def __init__(self) -> None:
-        pass
+        self.conn = psycopg2.connect(
+            database="books_mobile",
+            host="localhost",
+            user="books",
+            password="books",
+            port="5432"
+        )
+        self.database: cursor = self.conn.cursor()
+
+    def make_query(self, query_stmt: str) -> List[tuple]:
+        self.database.execute(query=query_stmt)
+
+        query_data = self.database.fetchall()
+        return query_data
