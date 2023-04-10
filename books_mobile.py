1e11cc45b4e8063f6d377d88705bc7ead35a479e
Lucas Rodrigues <lucas.darlindo@gmail.com>
2023-04-10 20:18:57 -0300

    feat: added endpoint for JWT Bearer Token (POST)

--- books_mobile.py
+++ books_mobile.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from typing import Annotated
+
+from starlette.requests import Request
+from fastapi import FastAPI, Body, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordRequestForm
 from loguru import logger
 
 from api.gateway.database import DatabaseAPI
+from api.controller.auth import BooksAuthController
+from api.model.user import Token
 
 methods_meta = [
+    {
+        "name": "Authentication",
+        "description": "Section dedicated for Authentication.",
+    },
     {
         "name": "Users",
         "description": "Section dedicated for Users Management.",
@@ -42,3 +52,19 @@ def test_database_connection() -> bool:
     except Exception as e:
         logger.error(f"Database connection error. Error/Exception: {e}.")
         return False
+
+
+@app.post(path="/auth-user", response_model=Token, tags=["Users", "Authentication"])
+async def user_login(user_credentials: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user = BooksAuthController().authenticate_user(
+        username=user_credentials.username, password=user_credentials.password
+    )
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+
+    access_token = BooksAuthController().create_access_token(data={"sub": user.name})
+    return {"access_token": access_token, "token_type": "bearer", "expiration_time": None}
