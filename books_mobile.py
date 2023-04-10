b2fd16e7bcec95544639ce76990ed2e5a8ed2b9f
Lucas Rodrigues <lucas.darlindo@gmail.com>
2023-04-10 16:04:02 -0300

    feat: added endpoint for DB CX Testing

--- books_mobile.py
+++ books_mobile.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
+from loguru import logger
 
+from api.gateway.database import DatabaseAPI
 
 methods_meta = [
     {
@@ -26,3 +28,13 @@ app = FastAPI(
     docs_url=None, redoc_url="/documentation",
     openapi_tags=methods_meta, description=description
 )
+
+
+@app.get(path="/test-db")
+def test_db_connection() -> bool:
+    try:
+        DatabaseAPI()
+        return True
+    except Exception as e:
+        logger.error(f"Database connection error. Error/Exception: {e}.")
+        return False
