63697a15dfc94315b828a1a02cdb7bf114396f4e
Lucas Rodrigues <lucas.darlindo@gmail.com>
2023-04-11 13:30:10 -0300

    feat: added method for inserting new user on user controller

--- api/controller/user.py
+++ api/controller/user.py
@@ -1,12 +1,15 @@
 from typing import List
 
 from api.gateway.database import DatabaseAPI
+from api.model.user import NewUser
 
 
 class BooksUserController:
     def __init__(self) -> None:
         self.db = DatabaseAPI()
-        self.users = self.db.get_users()
 
     def get_users(self) -> List[tuple]:
-        return self.users
+        return self.db.get_data(table_name="users")
+
+    def insert_user(self, user_data: NewUser) -> bool:
+        return self.db.insert_data(table_name="users", data_dict=user_data.dict())
